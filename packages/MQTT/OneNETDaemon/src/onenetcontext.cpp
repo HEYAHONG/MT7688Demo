92a71b3bebb5b43da8a7962ffd19f92d50a01868
HEYAHONG <2229388563@qq.com>
2023-03-28 17:19:08 +0800

    减小CPU占用

--- packages/MQTT/OneNETDaemon/src/onenetcontext.cpp
+++ packages/MQTT/OneNETDaemon/src/onenetcontext.cpp
@@ -84,7 +84,7 @@ void OneNETContext::Run()
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(5));
         }
-        std::this_thread::yield();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
